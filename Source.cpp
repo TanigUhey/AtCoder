36397f2e606b6832f4ee820082f16edfb3876e42
taniguhey <whole.creation.mail@gmail.com>
2019-10-27 22:43:19 +0900

    テンプレートをアップデート
    bits/stdc++を読み込むように変更
    逆順forを追加
    型名の省略を導入
    gcd, printを導入

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,23 @@
-#include <iostream>
-#include <cstdio>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <map>
+#include <bits/stdc++.h>
 
 #define INF 2e9
 
 #define REP(i,n) for(int i = 0; i < n;++i)
 #define REPA(i,a,n) for(int i = a; i < n; ++i)
+#define REPR(i,a) for(int i = a; i >= 0; --i)
 #define ALL(a) a.begin(), a.end()
 
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector< vector<int> > vvi;
+
+int gcd(int a, int b){ return b ? gcd(b, a % b) : a; }
+template<typename A> void psn(A a){ cout << a << endl; }
+template<typename A, typename... B> void psn(A a, B... b){ cout << a << ' '; psn(b...); }
+
 int main(void) {
 
-
-
-	return 0;
+    return 0;
 }
